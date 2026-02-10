33536a849aba9550fbb350922cdf7b2619550985
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2026-02-10 09:07:10 +0000

    Edited Spark

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -4,18 +4,18 @@ import { Toaster as Sonner, ToasterProps } from "sonner"
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
-      theme="dark"
-      className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as CSSProperties
+          "--norma
       }
-      {...props}
     />
-  )
 }
-
 export { Toaster }
+
+
+
+
+
+
+
+
+
+
